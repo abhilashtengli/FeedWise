1535030dad81c0b674c53e1865eb28052480422d
Abhilash <abhilasht0007.t@gmail.com>
2025-01-08 11:46:56 +0530

    DB connection.

--- lib/database.ts
+++ lib/database.ts
@@ -1,7 +1,17 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.mongoURL as string);
+  if (mongoose.connections[0].readyState) {
+    console.log("Connection is ready");
+    return true;
+  }
+  try {
+    await mongoose.connect(process.env.mongoURL as string);
+    console.log("Connection is successfully established");
+  } catch (err) {
+    console.log("Something went wrong Database connection failed : " + err);
+    process.exit();
+  }
 };
 
 module.exports = connectDB;
--- models/User.ts
+++ models/User.ts
@@ -1,27 +1,30 @@
 import mongoose from "mongoose";
 import validator from "validator";
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    minLength: 2
-  },
-  email: {
-    type: String,
-    lowercase: true,
-    required: true,
-    unique: true,
-    trim: true,
-    email: true,
-    validate(value: string) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Invalid email address");
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 2
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+      email: true,
+      validate(value: string) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address");
+        }
       }
     }
-  }
-});
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 module.exports = User;
